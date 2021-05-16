70bf4b6d5c5e386fe8b19e1cb9e694bb2fd88c62
ArishaMukhtar19 <84094197+ArishaMukhtar19@users.noreply.github.com>
2021-05-16 21:23:26 +0500

    First Commit

---
+++ Calculator/Calc_main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calc_main : Operations
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double num1, num2;
+                string num1temp, num2temp;
+                bool isDouble;
+                while (true)
+                {
+                    Console.Write("\n\nEnter First Number: ");
+                    num1temp = Console.ReadLine();
+                    isDouble = double.TryParse(num1temp, out num1);
+                    if (isDouble)
+                        break;
+                    else
+                        Console.Write("\n\nFormat Error! Please Give a number/numeric input");
+
+                }
+                while (true)
+                {
+                    Console.Write("\n\nEnter Second Number: ");
+                    num2temp = Console.ReadLine();
+                    isDouble = double.TryParse(num2temp, out num2);
+                    if (isDouble)
+                        break;
+                    else
+                        Console.Write("\n\nFormat Error! Please Give a number/numeric input");
+
+                }
+                Operations obj = new Operations();
+                obj.Calculate(num1, num2);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n\nError! " + e.Message);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ Calculator/Operations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Operations
+    {
+        public void Calculate(double num1, double num2)
+        {
+            bool isInt;
+            string operationTemp;
+            int operation=0;
+            double result=0;
+            while(true)
+            {
+                Console.Write("\n\nPlease Enter which operation you want to Evaluate\n\n1--Add\n2--Subtract\n3--Multiply\n4--Divide\n\nEnter Choice: ");
+                operationTemp = Console.ReadLine();
+                isInt = int.TryParse(operationTemp, out operation);                
+                if(operation>4 || operation<1)
+                    Console.Write("\n\nWrong input! Please provide a correct choice!");
+                else if (isInt)
+                    break;
+                else
+                    Console.Write("\n\nWrong input! Please provide a correct choice!");
+            }
+            switch(operation)
+            {
+                case 1:
+                    result = num1 + num2;
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    break;
+                case 4:
+                    result = num1 / num2;
+                    break;
+            }
+            Console.Write("\n\nEvaluated Result is: " + result);
+
+        }
+    }
+}
